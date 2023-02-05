756bfa78c55c3f8516307486d4467aa7e58d8a04
Alisina Barghandan <barghandanalisina@gmail.com>
2023-02-05 16:09:26 +0530

    Create header.php

---
+++ modules/header.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <span class="fs-4">UserCrud</span>
+        </a>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="create_new_user.php" class="nav-link">Create new user</a></li>
+            <li class="nav-item"><a href="all_users.php" class="nav-link">All users</a></li>
+        </ul>
+    </header>
+</div>
